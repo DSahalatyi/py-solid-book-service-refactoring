0c5251949056a3c721fc632619a0bd2194cff9b9
DSahalatyi <dsahalatyi@gmail.com>
2024-10-29 17:25:47 +0200

    Fix linter error

--- app/display.py
+++ app/display.py
@@ -1,7 +1,5 @@
 from abc import abstractmethod, ABC
 
-from app.book import Book
-
 
 class Display(ABC):
     @abstractmethod
--- app/main.py
+++ app/main.py
@@ -7,13 +7,16 @@ from app.serializer import JsonSerializer, XmlSerializer
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
     for cmd, method_type in commands:
         if cmd == "display":
-            display_strategy = ConsoleDisplay() if method_type == "console" else ReverseDisplay()
+            display_strategy = ConsoleDisplay()\
+                if method_type == "console"else ReverseDisplay()
             display_strategy.display(book.content)
         elif cmd == "print":
-            printer = ConsolePrinter() if method_type == "console" else ReversePrinter()
+            printer = ConsolePrinter()\
+                if method_type == "console" else ReversePrinter()
             printer.print_book(book)
         elif cmd == "serialize":
-            serializer = JsonSerializer() if method_type == "json" else XmlSerializer()
+            serializer = JsonSerializer()\
+                if method_type == "json" else XmlSerializer()
             return serializer.serialize(book)
 
 
